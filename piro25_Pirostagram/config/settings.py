d28256ad969ce6b140add0f0b482ed3984a0e923
borissal1207 <borissal127@gmail.com>
2026-07-09 17:00:49 +0900

    docker:Dockerfile, docker-composer 작성, mysql 연동

--- piro25_Pirostagram/config/settings.py
+++ piro25_Pirostagram/config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,8 +77,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.environ.get("MYSQL_DATABASE", "pirostagram_db"),
+        "USER": os.environ.get("MYSQL_USER", "pirostagram_user"),
+        "PASSWORD": os.environ.get("MYSQL_PASSWORD", "pirostagram_password"),
+        "HOST": os.environ.get("MYSQL_HOST", "db"),
+        "PORT": os.environ.get("MYSQL_PORT", "3306"),
+        "OPTIONS": {
+            "charset": "utf8mb4",
+        },
     }
 }
 
