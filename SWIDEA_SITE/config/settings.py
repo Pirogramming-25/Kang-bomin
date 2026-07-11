e12105b0c980ec3de6c330ed11db1b4a5e9833a2
borissal1207 <borissal127@gmail.com>
2026-07-11 16:12:39 +0900

    feat: Deploy

--- SWIDEA_SITE/config/settings.py
+++ SWIDEA_SITE/config/settings.py
@@ -20,12 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z^ap2xix3ge^h6w6!w%0fd+#dm-d%p#2woc%h#grf4_#sn@wsl'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
